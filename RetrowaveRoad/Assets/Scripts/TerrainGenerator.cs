8b5f8dccef8ea7d16c1ae59adb4c60fb8e0e6b16
Scott Fitzpatrick <sfitzpatrick@chapman.edu>
2023-01-03 15:02:58 -0800

    Optimization & aesthetics
    -Visualizer meshes are now combined to reduce draw calls. Performance increased by around 600%.
    -Tweaks to aesthetics
    -Compressed HDRI now included
    
    TODO:
    -Implement mesh combiner into GenerateNewTerrain() so that meshes can be combined during runtime for optimization.

--- RetrowaveRoad/Assets/Scripts/TerrainGenerator.cs
+++ RetrowaveRoad/Assets/Scripts/TerrainGenerator.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 using static UnityEditor.PlayerSettings;
 
 [RequireComponent(typeof(MeshFilter))]
@@ -19,30 +21,29 @@ public class TerrainGenerator : MonoBehaviour
     [Range(0.01f, 3f)] public float valleyAverageIncline = 0.4f;
     [Range(0.01f, 10f)] public float valleySubtractIncline = 1.3f;
     [Range(0f, 25f)] public float floorHeight = 1f;
-    [Range(0f, 25f)] public float speed = 5f;
+    [Range(0f, 50f)] public float speed = 5f;
     public bool testValuesRealtime = false;
 
     [Header("Vertex Properties")]
     public bool visualizeVerts = false;
     public GameObject vertVisualizer;
-    [Range(0.001f, 0.25f)] public float vertScale = 0.025f;
+    [Range(0.5f, 5f)] public float vertScale = 5f;
 
     [Header("Line Properties")]
     public bool visualizeLines = false;
     public GameObject lineVisualizer;
-    [Range(0.001f, 0.1f)] public float lineThickness = 0.025f;
+    [Range(0.5f, 2.5f)] public float lineThickness = 1f;
+
+    [Header("Mesh Combine")]
+    public GameObject combinedMeshContainer;
 
     private Mesh mesh;
     private List<Vector3> vertices;
     private List<int> triangles;
     private GameObject vertSpheres;
     private GameObject lineCylinders;
-
-    private GameObject sphereContainer;
-    private GameObject cylinderContainer;
-    //private GameObject[] vertSphereBatch;
-    //private GameObject[] lineCylinderBatch;
-
+    private GameObject combinedSpheres;
+    private List<CombineInstance[]> combineInstances;
 
     private int vert;
     private int backVertZPos;
@@ -56,9 +57,7 @@ public class TerrainGenerator : MonoBehaviour
      *   -Add newly generated objects to batches at runtime
      *   -Delay mesh spawning
      * -Add wireframe lines
-     * -Make camera script. Move it from there.
      * -Adjust terrain shader
-     * -Add sun and HDRI sky
      * -Re-color
      * -Finishing touches such as: air particles, fog, vert flicker, etc.
      * -Clean up code
@@ -70,39 +69,39 @@ public class TerrainGenerator : MonoBehaviour
 
     void Start()
     {
+        // Prevent terrain from moving if in testing mode
+        if (testValuesRealtime)
+        {
+            Debug.Log("TESTING VALUES - Visualizers disabled and speed set to 0.");
+            speed = 0;
+            visualizeVerts = false;
+            visualizeLines = false;
+        }
+
         // Create child GO for grandparent vert spheres
         vertSpheres = new GameObject();
         vertSpheres.name = "Vertices";
         vertSpheres.transform.parent = transform;
         vertSpheres.isStatic = true;
-        
+
         // Create child GO for grandparent line cylinders
         lineCylinders = new GameObject();
         lineCylinders.name = "Lines";
         lineCylinders.transform.parent = transform;
         lineCylinders.isStatic = true;
-        
+
+        // Create child GO for combined meshes
+        //combinedSpheres = new GameObject();
+        //combinedSpheres.name = "CombinedVertices";
+        //combinedSpheres.transform.parent = transform;
+        //combinedSpheres.isStatic = true;
+
         // Init mesh
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         backVertZPos = zSize;
         frontVertZPos = 0;
         InitTerrain();
-
-        StaticBatchingUtility.Combine(vertSpheres);
-        StaticBatchingUtility.Combine(lineCylinders);
-
-        //// Create batch container for verts
-        //sphereContainer = new GameObject();
-        //sphereContainer.name = "VertContainer";
-        //sphereContainer.isStatic = true;
-        //StaticBatchingUtility.Combine(ChildrenToArray(vertSpheres), sphereContainer);
-
-        //// Create batch container for cylinders
-        //cylinderContainer = new GameObject();
-        //cylinderContainer.name = "VertContainer";
-        //cylinderContainer.isStatic = true;
-        //StaticBatchingUtility.Combine(ChildrenToArray(lineCylinders), cylinderContainer);
     }
 
     void Update()
@@ -115,7 +114,6 @@ public class TerrainGenerator : MonoBehaviour
         camPos.z += Time.deltaTime * speed;
         Camera.main.transform.position = camPos;
 
-
         // GENERATE/DEGENERATE TERRAIN ---
         // If the camera is 5 in front of cur vert on z-axis
         if (Camera.main.transform.position.z - 5f > vertices[frontVertZPos].z)
@@ -124,7 +122,7 @@ public class TerrainGenerator : MonoBehaviour
             DegenerateTerrain();
 
             backVertZPos++;
-            frontVertZPos += xSize;
+            frontVertZPos += xSize + 1;
         }
 
         UpdateMesh();
@@ -147,13 +145,23 @@ public class TerrainGenerator : MonoBehaviour
                 if (visualizeVerts)
                     VisualizeVert(new Vector3(x, y, z));
 
-
-                //float distFromCam = Vector3.Distance(newPos, Camera.main.transform.position);
-                if (visualizeLines && x > 0)// && distFromCam < 100)
+                if (visualizeLines && x > 0)
                     VisualizeLine(newPos, prevPos);
 
                 prevPos = newPos;
             }
+        }
+
+        // Combine meshes to reduce batches
+        if (visualizeVerts)
+        {
+            CombineMeshes(vertSpheres);
+            vertSpheres.SetActive(false);
+        }
+        if (visualizeLines)
+        {
+            CombineMeshes(lineCylinders);
+            lineCylinders.SetActive(false);
         }
 
         // GENERATE TRIS ---
@@ -192,8 +200,7 @@ public class TerrainGenerator : MonoBehaviour
             if (visualizeVerts)
                 VisualizeVert(newPos);
 
-            //float distFromCam = Vector3.Distance(newPos, Camera.main.transform.position);
-            if (visualizeLines && x > 0)// && distFromCam < 100)
+            if (visualizeLines && x > 0)
                 VisualizeLine(newPos, prevPos);
 
             prevPos = newPos;
@@ -237,8 +244,6 @@ public class TerrainGenerator : MonoBehaviour
         mesh.triangles = triangles.ToArray();
     }
 
-    
-
     float CalculateHeight(int x, int z)
     {
         // Calculate noise height
@@ -246,7 +251,7 @@ public class TerrainGenerator : MonoBehaviour
 
         // Avoid any negative perlin vals
         curHeight = curHeight < 0 ? 0 : curHeight;
-        
+
         // Noise height multipliers
         curHeight *= height;
         curHeight = Mathf.Pow(curHeight, sharpness);
@@ -271,9 +276,6 @@ public class TerrainGenerator : MonoBehaviour
         curSphere.transform.parent = vertSpheres.transform;
         curSphere.transform.position = pos;
         curSphere.transform.localScale = new Vector3(vertScale, vertScale, vertScale);
-
-        //GameObject[] test = new GameObject[] { curSphere };
-        //StaticBatchingUtility.Combine(test, vertSpheres);
     }
 
     void VisualizeLine(Vector3 posA, Vector3 posB)
@@ -282,31 +284,59 @@ public class TerrainGenerator : MonoBehaviour
         curLine.transform.parent = lineCylinders.transform;
 
         float distance = Vector3.Distance(posA, posB);
-        curLine.transform.localScale = new Vector3(
-            lineThickness, distance / 2f, lineThickness);
+        curLine.transform.localScale = new Vector3(lineThickness, distance / 2f, lineThickness);
 
         Vector3 middlePoint = (posA + posB) / 2f;
         curLine.transform.position = middlePoint;
 
         Vector3 rotationDirection = posA - posB;
         curLine.transform.up = rotationDirection;
-
-        //GameObject[] test = new GameObject[] { curLine };
-        //StaticBatchingUtility.Combine(test, lineCylinders);
+        //curLine.transform.forward = rotationDirection;
 
         // Reference: https://www.youtube.com/watch?v=K-Nckj9tppM
     }
 
-    //GameObject[] ChildrenToArray(GameObject parent)
-    //{
-    //    Debug.Log(Time.time);
-    //    Transform[] childrenTrans = parent.GetComponentsInChildren<Transform>();
-    //    GameObject[] children = new GameObject[childrenTrans.Length];
+    void CombineMeshes(GameObject parent)
+    {
+        // CREATE LIST OF COMBINE INSTANCES =======================================================
+        MeshFilter[] meshFilters = parent.GetComponentsInChildren<MeshFilter>();
 
-    //    for (int i = 0; i < childrenTrans.Length; i++)
-    //        children[i] = childrenTrans[i].gameObject;
+        combineInstances = new List<CombineInstance[]>();
+        CombineInstance[] curCombine = new CombineInstance[1000];
 
-    //    Debug.Log(Time.time);
-    //    return children;
-    //}
+        int meshesLeft = meshFilters.Length;
+        int i = 0;
+        while (meshesLeft > 1000)
+        {
+            for (int j = 0; j < 1000; j++)
+            {
+                curCombine[j].mesh = meshFilters[i].sharedMesh;
+                curCombine[j].transform = meshFilters[i + j].transform.localToWorldMatrix;
+            }
+
+            combineInstances.Add(curCombine);
+            curCombine = new CombineInstance[1000]; //reset
+
+            meshesLeft -= 1000;
+            i += 1000;
+        }
+
+        // CREATE COMBINED MESHES WITH COMBINE INSTANCES ==========================================
+        for (int k = 0; k < combineInstances.Count; k++)
+        {
+            GameObject newCombine = Instantiate(combinedMeshContainer);
+
+            newCombine.name = "CombinedMesh_" + k;
+            //newCombine.transform.parent = combinedSpheres.transform;
+            newCombine.GetComponent<MeshFilter>().mesh = new Mesh();
+            newCombine.GetComponent<MeshFilter>().mesh.CombineMeshes(combineInstances[k]);
+            newCombine.GetComponent<MeshRenderer>().material = 
+                parent.transform.GetChild(0).GetComponent<MeshRenderer>().material; 
+            newCombine.SetActive(true);
+
+            newCombine.transform.localScale = Vector3.one;
+            newCombine.transform.rotation = Quaternion.identity;
+            newCombine.transform.position = Vector3.zero;
+        }
+    }
 }
